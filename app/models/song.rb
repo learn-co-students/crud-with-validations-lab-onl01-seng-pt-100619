fd10d91da541a81be0f9917163c8ffa5981cd717
Phil Billingsby <philbillingsby@gmail.com>
2020-03-06 19:21:18 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,16 @@
 class Song < ApplicationRecord
   validates :title, presence: true
   validates :title, uniqueness: {scope: :release_year}
-  validates :release_year, presence: true, unless: :released?
-  validates :release_year, numericality: {less_than: Time.now.year}
+  validate :method
+  # validates :release_year, numericality: {less_than: Time.now.year}
+
+  def method
+    if release_year.nil? && released == true
+      errors.add(:release_year, "Release year must be added")
+    elsif release_year.nil? && released == false
+      nil
+    elsif release_year > Time.now.year
+      errors.add(:release_year, "Release year must be before #{Time.now.year}")
+    end
+  end
 end
