c73faa8cb1b89dd394ba051997664adc4f9a6e0e
emanningt <emanningmusic@yahoo.com>
2020-03-29 14:45:20 -0700

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,20 @@
+class Song < ApplicationRecord
+    validates :title, presence: true
+  validates :title, :uniqueness => { :scope => [:artist_name, :release_year]}
+  validates :released, inclusion: { in: [true, false] }
+  validates :release_year, presence: true, if: :released 
+  validate :max_year?
+  validates :artist_name, presence: :true
+
+  def max_year?
+    if self.release_year
+      if release_year <= Date.current.year
+        return true
+      else
+        errors.add(:release_year, "no time traveling")
+      end
+    else
+      false
+    end
+  end
+end
